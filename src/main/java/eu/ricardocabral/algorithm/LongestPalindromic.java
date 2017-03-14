accca9a5bee998ed8e6fe4f7b246a98b8545b858
Ricardo Zanquini Cabral <Ricardo Zanquini Cabral>
2017-03-14 22:47:27 +0100

    Added Manacher class

---
+++ src/main/java/eu/ricardocabral/algorithm/LongestPalindromic.java
@@ -0,0 +1,14 @@
+package eu.ricardocabral.algorithm;
+
+/**
+ * 
+ * Manacher (1975) found a linear time algorithm for listing palindromes that
+ * appear at the start of a given string- O(n)
+ * 
+ * @author Ricardo
+ *
+ */
+public class LongestPalindromic {
+
+	String s = "HYTBCABADEFGHABCDEDCBAGHTFYW1234567887654321ZWETYGDE";
+}
