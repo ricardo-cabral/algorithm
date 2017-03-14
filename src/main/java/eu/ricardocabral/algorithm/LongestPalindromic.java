775e39c77d9f7813ef93c76c2e6cfbfadf984f48
Ricardo Zanquini Cabral <Ricardo Zanquini Cabral>
2017-03-14 23:02:05 +0100

    Added links to help

--- src/main/java/eu/ricardocabral/algorithm/LongestPalindromic.java
+++ src/main/java/eu/ricardocabral/algorithm/LongestPalindromic.java
@@ -5,10 +5,15 @@ package eu.ricardocabral.algorithm;
  * Manacher (1975) found a linear time algorithm for listing palindromes that
  * appear at the start of a given string- O(n)
  * 
+ * texts to understand:
+ * http://articles.leetcode.com/longest-palindromic-substring-part-i
+ * http://articles.leetcode.com/longest-palindromic-substring-part-ii
+ * https://www.youtube.com/watch?v=nbTSfrEfo6M
  * @author Ricardo
  *
  */
 public class LongestPalindromic {
 
 	String s = "HYTBCABADEFGHABCDEDCBAGHTFYW1234567887654321ZWETYGDE";
+	String s2 = "abababa";
 }
