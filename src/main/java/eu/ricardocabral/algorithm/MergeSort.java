b4c29de3f8445576692ca653a40ae989edf70276
Ricardo Zanquini Cabral <Ricardo Zanquini Cabral>
2017-04-10 21:17:23 +0200

    mergesort book.

--- src/main/java/eu/ricardocabral/algorithm/MergeSort.java
+++ src/main/java/eu/ricardocabral/algorithm/MergeSort.java
@@ -21,29 +21,28 @@ public class MergeSort {
 		mergeSort(array, 0, array.length - 1);
 		System.out.println("Sorted array: " + Arrays.toString(array));
 
-		int[] myArray = new int[] { 45, 3, 50, 1, 20, 15, 33, 35};
+		int[] myArray = new int[] { 45, 3, 50, 1, 20, 15, 33, 35 };
 		tmpArray = new int[myArray.length];
 		System.out.println("My Merge sort: ");
-		myMergeSort(myArray, 0, myArray.length -1);
+		myMergeSort(myArray, 0, myArray.length - 1);
 		System.out.println(Arrays.toString(myArray));
-		
-		int[] arrayToMerge = new int[]{8,4};
-		myMerge(arrayToMerge, 0, 0, 1);
-		
 
-		int[] arrayToMerge2 = new int[]{4,8,9};
+		int[] arrayToMerge = new int[] { 8, 4 };
+		myMerge(arrayToMerge, 0, 0, 1);
+
+		int[] arrayToMerge2 = new int[] { 4, 8, 9 };
 		myMerge(arrayToMerge2, 1, 1, 2);
-		
-		int[] arrayToMerge3 = new int[]{3,41,52,1};
+
+		int[] arrayToMerge3 = new int[] { 3, 41, 52, 1 };
 		myMerge(arrayToMerge3, 0, 2, 3);
-		
-		//int[] bookArray = new int[] { 45, 3, 50, 1, 20, 15, 33, 35};
-		int[] bookArray = new int[] { 3,41,52,1};
+
+		// int[] bookArray = new int[] { 45, 3, 50, 1, 20, 15, 33, 35};
+		int[] bookArray = new int[] { 45, 3, 50, 1, 20, 15, 33, 35 };
 		System.out.println("Book Merge sort: ");
-		//bookMergeSort(bookArray, 0, bookArray.length -1);
-		bookMerge(bookArray, 0, 2, 3);
+		bookMergeSort(bookArray, 0, bookArray.length - 1);
+		// bookMerge(bookArray, 0, 2, 3);
 		System.out.println(Arrays.toString(bookArray));
-		
+
 	}
 
 	private static void mergeSort(int[] array, int begin, int end) {
@@ -87,25 +86,28 @@ public class MergeSort {
 		int middle;
 		if (begin < end) {
 			middle = (end + begin) / 2;
-			/*System.out.println(Arrays.toString(Arrays.copyOfRange(array, begin, end)) + "- begin= " + begin
-					+ ". middle= " + middle + ", end = " + end);*/
-			//Sort the left side of the array
+			/*
+			 * System.out.println(Arrays.toString(Arrays.copyOfRange(array,
+			 * begin, end)) + "- begin= " + begin + ". middle= " + middle +
+			 * ", end = " + end);
+			 */
+			// Sort the left side of the array
 			myMergeSort(array, begin, middle);
-			//sort the right side of the array
+			// sort the right side of the array
 			myMergeSort(array, middle + 1, end);
-			//combine both arrays
+			// combine both arrays
 			myMerge(array, begin, middle, end);
 		}
 	}
 
 	private static void myMerge(int[] array, int begin, int middle, int end) {
-		helper = new int[end+1];
+		helper = new int[end + 1];
 		for (int i = begin; i <= end; i++) {
 			helper[i] = array[i];
 		}
 
 		int beginAux = begin;
-		int middleAux = middle +1;
+		int middleAux = middle + 1;
 		int beginAux2 = begin;
 
 		while (beginAux <= middle && middleAux <= end) {
@@ -127,61 +129,53 @@ public class MergeSort {
 		System.out.println("Merge: " + Arrays.toString(array));
 	}
 
-	private static void bookMergeSort(int[] array, int begin, int end){
-		int middle;
+	private static void bookMergeSort(int[] array, int begin, int end) {
+
 		if (begin < end) {
-			middle = (end + begin) / 2;
-			/*System.out.println(Arrays.toString(Arrays.copyOfRange(array, begin, end)) + "- begin= " + begin
-					+ ". middle= " + middle + ", end = " + end);*/
-			//Sort the left side of the array
+			int middle = (end + begin) / 2;
+			/*
+			 * System.out.println(Arrays.toString(Arrays.copyOfRange(array,
+			 * begin, end)) + "- begin= " + begin + ". middle= " + middle +
+			 * ", end = " + end);
+			 */
+			// Sort the left side of the array
 			bookMergeSort(array, begin, middle);
-			//sort the right side of the array
+			// sort the right side of the array
 			bookMergeSort(array, middle + 1, end);
-			//combine both arrays
+			// combine both arrays
 			bookMerge(array, begin, middle, end);
 		}
 	}
-	private static void bookMerge(int[] array, int begin, int middle, int end){
-		
-		int lengthLeft = middle -begin +1 ;
+
+	private static void bookMerge(int[] array, int begin, int middle, int end) {
+
+		int lengthLeft = middle - begin + 1;
 		int lengthRight = end - middle;
-		
-		int[] right = new int[array.length];
-		int[] left = new int[array.length];
-		
-		for(int i = 0; i< lengthLeft; i++){
+
+		int[] left = new int[lengthLeft + 1];
+		int[] right = new int[lengthRight + 1];
+
+		for (int i = 0; i < lengthLeft; i++) {
 			left[i] = array[begin + i];
 		}
-		
-		for(int j = middle; j < end; j++){
-			right[j] = array[middle + 1];
+
+		for (int j = 0; j < lengthRight; j++) {
+			right[j] = array[middle + j + 1];
 		}
-		
-		int pointLeft = 0, pointRigth = 0, pointArray = 0;
-		
-		while(pointLeft < lengthLeft && pointRigth < lengthRight){
-			if(left[pointLeft] < right[pointRigth]){
-				array[pointArray] = left[pointLeft];
-				pointLeft ++;
-			}else{
-				array[pointArray] = right[pointRigth];
-				pointRigth++;
+
+		left[lengthLeft] = Integer.MAX_VALUE;
+		right[lengthRight] = Integer.MAX_VALUE;
+
+		for (int pointLeft = 0, pointRight = 0, pointArray = begin; pointArray <= end; pointArray++) {
+			if (left[pointLeft] <= right[pointRight]) {
+				array[pointArray] = left[pointLeft++];
+
+			} else {
+				array[pointArray] = right[pointRight++];
+
 			}
-			pointArray++;
 		}
-		
-		while(pointLeft < lengthLeft){
-			array[pointArray] = left[pointLeft];
-			pointLeft++;
-			pointArray++;
-		}
-		
-		while(pointRigth < lengthRight){
-			array[pointArray] = right[pointRigth];
-			pointRigth++;
-			pointArray++;
-		}
+
 	}
-	
 
 }
---
+++ src/main/java/eu/ricardocabral/algorithm/TestMergeSort.java
@@ -0,0 +1,61 @@
+package eu.ricardocabral.algorithm;
+
+import java.util.Arrays;
+
+public class TestMergeSort {
+	
+	public static void main(String[] args) {
+		int[] array = new int[] {45, 3, 50, 1, 20, 15, 33, 35 };
+		 mergeSort(array, 0, array.length - 1);
+		 System.out.println(Arrays.toString(array));
+	}
+
+	
+	/**
+     * Recursive merge sort procedure to sort the subarray
+     * @param array The array containing the subarray to be sorted.
+     * @param p Index of the beginning of the subarray.
+     * @param r Index of the end of the subarray.
+     */
+    private static void mergeSort(int[] array, int p, int r){
+        if(p < r){
+            int q = (p + r) / 2;
+            mergeSort(array, p, q);
+            mergeSort(array, q + 1, r);
+            merge(array, p, q, r);
+        }
+    }
+
+    /**
+     * @param array The array containing the subarrays to be merged.
+     * @param p Index of the beginning of the first subarray.
+     * @param q Index of the end of the first subarray; the second
+     * subarray starts at index q+1.
+     * @param r Index of the end of the second subarray.
+     */
+    private static void merge(int[] array, int p, int q, int r){
+        int n1 = q - p + 1;
+        int n2 = r - q;
+        int[] left = new int[n1 + 1];
+        int[] right = new int[n2 + 1];
+
+        for(int i = 0; i < n1; i++){
+            left[i] = array[p + i];
+        }
+
+        for(int j = 0; j < n2; j++){
+            right[j] = array[q + j + 1];
+        }
+
+        left[n1] = Integer.MAX_VALUE;	// null indicates infinity
+        right[n2] = Integer.MAX_VALUE;
+
+        for(int i = 0,  j = 0,  k = p; k <= r; k++){
+            if(left[i] <= right[j]){
+                array[k] = left[i++];
+            }else{
+                array[k] = right[j++];
+            }
+        }
+    }
+}
